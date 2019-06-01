b054867cc59a80c32ccd2fa61470bc4e68f32bdc
John Slowik <john@slowiklabs.com>
2019-06-01 16:58:41 -0500

    Framework created for using UPC api

---
+++ EntertainmentDatabase.Core/Dto/UpcItemDbDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntertainmentDatabase.Core.Dto
+{
+    public class UpcItemDbDetailsDto
+    {
+        public string Title { get; set; }
+        public string Ean { get; set; }
+        public string Upc { get; set; }
+        public string Description { get; set; }
+    }
+}
