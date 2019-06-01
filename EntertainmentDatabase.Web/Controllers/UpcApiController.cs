e2cb598354b06c4c9d35fcf189652d92c0ccb90a
John Slowik <john@slowiklabs.com>
2019-06-01 17:07:13 -0500

    Json annotation

--- EntertainmentDatabase.Web/Controllers/UpcApiController.cs
+++ EntertainmentDatabase.Web/Controllers/UpcApiController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EntertainmentDatabase.Web.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class UpcApiController : Controller
     {
         [HttpGet("[action]")]
