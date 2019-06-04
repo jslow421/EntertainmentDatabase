1a2468f035dd6140a61d3524b786d34a6afe5418
Unknown <dunlap.joe@gmail.com>
2019-06-03 22:16:36 -0500

    Added properites to book
    i added some properties to the book.

--- EntertainmentDatabase.Core/Entities/BoardGame.cs
+++ EntertainmentDatabase.Core/Entities/BoardGame.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,6 @@ namespace EntertainmentDatabase.Core.Entities
 {
     class BoardGame
     {
+
     }
 }
