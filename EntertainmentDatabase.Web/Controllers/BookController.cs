06a0213f22e8e13112e8b231cecb9f35645c4925
Unknown <dunlap.joe@gmail.com>
2019-06-17 22:14:51 -0500

    Added the right controller
    JOhn im going nuts with commits.

--- EntertainmentDatabase.Web/Controllers/BookController.cs
+++ EntertainmentDatabase.Web/Controllers/BookController.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EntertainmentDatabase.Web.Controllers
 {
-    public class BookController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        
     }
 }
