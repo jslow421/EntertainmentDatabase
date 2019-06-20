829da5a001a77d30d4d61255183293c4f2eb7830
John Slowik <john@slowiklabs.com>
2019-06-20 00:33:08 -0500

    Updating UPC api test to be slightly more useful
    Using response code to verify connectivity

--- EntertainmentDatabase.IntegrationTests/ExternalApiIntegrationTests.cs
+++ EntertainmentDatabase.IntegrationTests/ExternalApiIntegrationTests.cs
@@ -8,11 +8,10 @@ namespace EntertainmentDatabase.IntegrationTests
         [Fact]
         public async void TestUpcItemConnection()
         {
-            // todo If keeping this test it will need to be updated. Will essentially always pass
             var upcDataManager = new UpcDataManager();
             var result = await upcDataManager.GetItemDetailsFromExternalApi("786936816365");
             
-            Assert.NotNull(result);
+            Assert.True(result.Code.Equals("OK"));
         }
     }
 }
