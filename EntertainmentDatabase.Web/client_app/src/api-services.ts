d8b3d447c19fc63510fa21aa82e44bac343c6604
John Slowik <john@slowiklabs.com>
2019-08-11 17:00:45 -0500

    Relative path to api

--- EntertainmentDatabase.Web/client_app/src/api-services.ts
+++ EntertainmentDatabase.Web/client_app/src/api-services.ts
@@ -1,16 +1,14 @@
 import axios from "axios";
 
-const API_URL = "https://localhost:5001";
-
 export class MovieRestApiService {
   
   public static getMovieByUpc(dto: object) {
-    const url = API_URL + "/api/UpcApi/GetDetailsByUpc";
+    const url = "../api/UpcApi/GetDetailsByUpc";
     return axios.post(url, dto).then(response => response.data).catch((error) => console.log(error));
   }
   
   public static saveMovie(dto: object) {
-    const url = API_URL + "/api/UpcApi/SaveNewMovie";
+    const url = "../api/UpcApi/SaveNewMovie";
     return axios.post(url, dto).then(response => response.data);
   }
 }
